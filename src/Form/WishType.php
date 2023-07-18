dc4127a62997ecbceeee92321c301376968c2bd1
Gregoire-Dubois <65712692+Gregoire-Dubois@users.noreply.github.com>
2023-07-18 10:49:31 +0200

    début TP 7

---
+++ src/Form/WishType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Wish;
+use Symfony\Component\Form\AbstractType;
+//use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class WishType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Title'
+            ] )
+            ->add('description', TextareaType::class, [
+                'required' => false,
+            ])
+            ->add('author')
+            //->add('isPublished')
+            /*->add('dateCreated', DateType::class, [
+               'html5' => true,
+               'widget' => 'single_text'
+           ])
+            */
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Wish::class,
+        ]);
+    }
+}
